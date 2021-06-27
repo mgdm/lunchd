d0b4d1f45bcd7b5d4ea3e0714c7b41ea15a04221
Michael Maclean <michael@mgdm.net>
2021-06-27 22:42:01 +0100

    Enable TLS, use port 443

--- main.go
+++ main.go
@@ -1,26 +1,28 @@
 package main
 
 import (
-        "fmt"
-        "log"
-        "math/rand"
-        "net"
-        "net/http"
-        "time"
+	"crypto/tls"
+	"flag"
+	"fmt"
+	"log"
+	"math/rand"
+	"net"
+	"net/http"
+	"time"
 
-        "github.com/coreos/go-systemd/activation"
+	"github.com/coreos/go-systemd/activation"
 )
 
 var lunchOptions = []string{
-        "Sandwich",
-        "Soup",
-        "Salad",
-        "Burger",
-        "Sushi",
+	"Sandwich",
+	"Soup",
+	"Salad",
+	"Burger",
+	"Sushi",
 }
 
 func getRandomLunch() string {
-        return lunchOptions[rand.Intn(len(lunchOptions))]
+	return lunchOptions[rand.Intn(len(lunchOptions))]
 }
 
 func getListener() (net.Listener, error) {
@@ -36,19 +38,63 @@ func getListener() (net.Listener, error) {
 	return listeners[0], err
 }
 
+func getCertificates() (string, string, error) {
+	keyPath := flag.String("key", "", "The path to the private key")
+	certPath := flag.String("certificate", "", "The path to the certificate")
+	flag.Parse()
+
+	if *keyPath == "" || *certPath == "" {
+		return "", "", fmt.Errorf("Either or both of -key or -cert not set")
+	}
+
+	return *keyPath, *certPath, nil
+}
+
+func tlsSetup(keyPath string, certPath string, listener net.Listener) (net.Listener, error) {
+	config := &tls.Config{
+		Certificates:             make([]tls.Certificate, 1),
+		NextProtos:               []string{"h2", "http/1.1"},
+		PreferServerCipherSuites: true,
+	}
+
+	var err error
+
+	log.Printf("Loading certs from key: %s and cert: %s", keyPath, certPath)
+
+	config.Certificates[0], err = tls.LoadX509KeyPair(
+		certPath,
+		keyPath,
+	)
+
+	if err != nil {
+		log.Printf("Failed to configure TLS: %s", err)
+		return nil, err
+	}
+
+	return tls.NewListener(listener, config), nil
+}
+
 func main() {
-        rand.Seed(time.Now().UnixNano())
+	rand.Seed(time.Now().UnixNano())
 
-        listener, err := getListener()
+	listener, err := getListener()
 
-        if err != nil {
-                log.Panicf("Could not set up listener: %s", err)
-        }
+	if err != nil {
+		log.Fatalf("Could not set up listener: %s", err)
+	}
 
-        http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-                fmt.Fprintf(w, "<h1>%s</h1>", getRandomLunch())
-        })
+	keyPath, certPath, err := getCertificates()
 
-        log.Printf("Starting web server on port %s", listener.Addr().String())
-        http.Serve(listener, nil)
+	if err != nil {
+		log.Fatalf("Could not load certificates: %s", err)
+	}
+
+	tlsListener, err := tlsSetup(keyPath, certPath, listener)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(w, "<h1>%s</h1>", getRandomLunch())
+	})
+
+	fmt.Printf("Starting web server on port %s", tlsListener.Addr().String())
+	http.Serve(tlsListener, nil)
 }
