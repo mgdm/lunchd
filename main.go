393af56196dd8f3cd654bece57ef7d840c88d5e9
Michael Maclean <michael@mgdm.net>
2021-07-19 10:33:17 +0100

    Run on HTTP and HTTPS at the same time

--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"crypto/tls"
+	"errors"
 	"flag"
 	"fmt"
 	"log"
 	"math/rand"
 	"net"
 	"net/http"
+	"os"
+	"sync"
 	"time"
 
 	"github.com/coreos/go-systemd/activation"
@@ -25,32 +28,43 @@ func getRandomLunch() string {
 	return lunchOptions[rand.Intn(len(lunchOptions))]
 }
 
-func getListener() (net.Listener, error) {
-	listeners, err := activation.Listeners()
+func getDefaultListeners() (map[string][]net.Listener, error) {
 
-	if err != nil || len(listeners) != 1 {
-		log.Printf("Excpected one listener, got %d: %s", len(listeners), err)
+	listener, err := net.Listen("tcp", ":8443")
 
-		listener, err := net.Listen("tcp", ":8080")
-		return listener, err
+	if err != nil {
+		return nil, err
 	}
 
-	return listeners[0], err
+	return map[string][]net.Listener{
+		"https": {listener},
+	}, nil
 }
 
-func getCertificates() (string, string, error) {
+func getListeners() (map[string][]net.Listener, error) {
+	listeners, err := activation.ListenersWithNames()
+
+	if err != nil || len(listeners) == 0 {
+		log.Printf("Received no listeners from socket activation, defaulting to HTTPS on port 8443")
+		listeners, err = getDefaultListeners()
+	}
+
+	return listeners, err
+}
+
+func getCertificatePaths() (string, string, error) {
 	keyPath := flag.String("key", "", "The path to the private key")
 	certPath := flag.String("certificate", "", "The path to the certificate")
 	flag.Parse()
 
 	if *keyPath == "" || *certPath == "" {
-		return "", "", fmt.Errorf("Either or both of -key or -cert not set")
+		return "", "", errors.New("Either or both of -key or -certificate not set")
 	}
 
 	return *keyPath, *certPath, nil
 }
 
-func tlsSetup(keyPath string, certPath string, listener net.Listener) (net.Listener, error) {
+func getTLSConfig(keyPath string, certPath string) (*tls.Config, error) {
 	config := &tls.Config{
 		Certificates:             make([]tls.Certificate, 1),
 		NextProtos:               []string{"h2", "http/1.1"},
@@ -71,30 +85,71 @@ func tlsSetup(keyPath string, certPath string, listener net.Listener) (net.Liste
 		return nil, err
 	}
 
-	return tls.NewListener(listener, config), nil
+	return config, nil
+}
+
+func createWebServers() (*http.ServeMux, *http.ServeMux) {
+	httpMux := http.NewServeMux()
+	httpMux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		log.Printf("HTTP request from %s\n", req.RemoteAddr)
+		hostname := fmt.Sprintf("https://%s", req.Host)
+		http.Redirect(w, req, hostname+req.RequestURI, http.StatusMovedPermanently)
+	})
+
+	httpsMux := http.NewServeMux()
+	httpsMux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		log.Printf("HTTPS request from %s\n", req.RemoteAddr)
+		fmt.Fprintf(w, "<h1>%s</h1>", getRandomLunch())
+	})
+
+	return httpMux, httpsMux
 }
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
-	listener, err := getListener()
+	log.Printf("LISTEN_FDS is %s\n", os.Getenv("LISTEN_FDS"))
+	log.Printf("LISTEN_FDNAMES is %s\n", os.Getenv("LISTEN_FDNAMES"))
+
+	listeners, err := getListeners()
+	httpMux, httpsMux := createWebServers()
 
 	if err != nil {
-		log.Fatalf("Could not set up listener: %s", err)
+		log.Fatalf("Could not set up listeners: %s", err)
 	}
 
-	keyPath, certPath, err := getCertificates()
+	var wg sync.WaitGroup
 
-	if err != nil {
-		log.Fatalf("Could not load certificates: %s", err)
+	if tlsListeners, ok := listeners["https"]; ok {
+		keyPath, certPath, err := getCertificatePaths()
+
+		if err != nil {
+			log.Fatalf("Could not load certificates: %s", err)
+		}
+
+		tlsConfig, err := getTLSConfig(keyPath, certPath)
+
+		for i := range tlsListeners {
+			wg.Add(1)
+
+			go func(l net.Listener) {
+				log.Printf("Starting secure web server on port %s\n", l.Addr())
+				tl := tls.NewListener(l, tlsConfig)
+				log.Fatal(http.Serve(tl, httpsMux))
+			}(tlsListeners[i])
+		}
 	}
 
-	tlsListener, err := tlsSetup(keyPath, certPath, listener)
+	if plainListeners, ok := listeners["http"]; ok {
+		for i := range plainListeners {
+			wg.Add(1)
 
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "<h1>%s</h1>", getRandomLunch())
-	})
+			go func(l net.Listener) {
+				log.Printf("Starting plaintext web server on port %s\n", l.Addr())
+				log.Fatal(http.Serve(l, httpMux))
+			}(plainListeners[i])
+		}
+	}
 
-	fmt.Printf("Starting web server on port %s", tlsListener.Addr().String())
-	http.Serve(tlsListener, nil)
+	wg.Wait()
 }
